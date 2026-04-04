e694b854f9d9502e510e7de61e48e7e8f69bea76
TaufiqStark <taufiguns@gmail.com>
2026-04-04 22:28:17 +0700

    feat: configure remote image domains and initialize project styling and landing page reference

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dweb.link',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bluulabs.s3.filebase.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
